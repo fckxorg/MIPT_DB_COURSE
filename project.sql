a3146ac07c17a9bddb590016422872e044c0fff0
Maxim Kokryashkin <32438596+fckxorg@users.noreply.github.com>
2021-04-06 23:43:28 +0300

    format fix

--- project.sql
+++ project.sql
@@ -17,37 +17,37 @@ create schema if not exists project;
 -- AVATAR TABLE
 create table project.avatar 
 (
-	avatar_id  		serial 			primary key,
-	avatar_url 		varchar(2048) 	not null 			-- 2048 is a maximum allowed size of http address
+	avatar_id  		serial 		primary key,
+	avatar_url 		varchar(2048) 	not null 							-- 2048 is a maximum allowed size of http address
 );
 
 -- USER TABLE
 create table project.user
 (
-	user_id 		serial 			primary key,
-	handle_txt 		varchar(256) 	not null unique,	-- Each user MUST have unique handle
+	user_id 		serial 		primary key,
+	handle_txt 		varchar(256) 	not null unique,						-- Each user MUST have unique handle
 											 	 		-- Limit of 256 characters is a common limit for handles
 	email_txt		varchar(320) 	unique,
-	avatar_id		integer			references project.avatar(avatar_id),
+	avatar_id		integer		references project.avatar(avatar_id),
 	bio_txt			text,
-	phone_no		varchar(20)		not null unique,	-- Each user must have phone number, 20 digits should be enough for everyone
+	phone_no		varchar(20)	not null unique,						-- Each user must have phone number, 20 digits should be enough for everyone
 	user_nm			varchar(256)	not null
 );
 
 -- STICKER TABLE
 create table project.sticker
 (
-	sticker_id		serial			primary key,
+	sticker_id		serial		primary key,
 	emotion_nm		varchar(256)	not null,
-	picture_url		varchar(2048)	not null			-- 2048 is a maximum allowed size of http address
+	picture_url		varchar(2048)	not null							-- 2048 is a maximum allowed size of http address
 ); 
 
 -- CHAT TABLE
 create table project.chat
 (
-	chat_id			serial			primary key,
-	avatar_id		integer			references project.avatar(avatar_id),
-	members_cnt		integer			not null,
+	chat_id			serial		primary key,
+	avatar_id		integer		references project.avatar(avatar_id),
+	members_cnt		integer		not null,
 	chat_nm			varchar(256)	not null
 );
 
@@ -57,10 +57,10 @@ create table project.message
 (
 	message_id		serial,
 	version_no		integer,
-	chat_id			integer			not null references project.chat(chat_id),
-	sender_id		integer			not null references project.user(user_id),
-	sticker_id		integer			references project.sticker(sticker_id),
-	timestamp_dttm	timestamp		not null,
+	chat_id			integer		not null references project.chat(chat_id),
+	sender_id		integer		not null references project.user(user_id),
+	sticker_id		integer		references project.sticker(sticker_id),
+	timestamp_dttm		timestamp	not null,
 	payload_txt		text
 );
 
@@ -70,20 +70,20 @@ add constraint PK_message primary key (message_id, version_no);
 -- CALL TABLE
 create table project.call
 (
-	call_id			serial			primary key,
-	chat_id			integer			not null references project.chat(chat_id),
+	call_id			serial		primary key,
+	chat_id			integer		not null references project.chat(chat_id),
 	duration_sec	interval		not null,
 	video_flg		boolean,
-	record_url		varchar(2048) 						-- 2048 is a maximum allowed size of http address
+	record_url		varchar(2048) 									-- 2048 is a maximum allowed size of http address
 );
 
 -- BOT TABLE
 create table project.bot
 (
-	bot_id			serial			primary key,
-	server_url		varchar(2048)	not null, 			-- 2048 is a maximum allowed size of http address
+	bot_id			serial		primary key,
+	server_url		varchar(2048)	not null, 							-- 2048 is a maximum allowed size of http address
 	bot_nm			varchar(256)	not null,
-	avatar_id		integer			references project.avatar(avatar_id)
+	avatar_id		integer		references project.avatar(avatar_id)
 );
 
 -- ATTACHMENT TABLE
@@ -92,9 +92,9 @@ create table project.attachment
 	attachment_id	serial			primary key,
 	message_id		integer,
 	version_no		integer,
-	type_txt		varchar(20)		not null,			-- file extension
-	file_url		varchar(2048)	not null, 			-- 2048 is a maximum allowed size of http address
-	size_amt		integer								-- file size in bytes
+	type_txt		varchar(20)	not null,							-- file extension
+	file_url		varchar(2048)	not null, 							-- 2048 is a maximum allowed size of http address
+	size_amt		integer										-- file size in bytes
 );
 
 alter table project.attachment
